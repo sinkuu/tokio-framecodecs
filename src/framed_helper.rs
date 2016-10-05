410b4034e8575c9fb082ea455351b5371d738a5c
sinkuu <sinkuupump@gmail.com>
2016-10-05 23:22:39 +0900

    Enumify

--- src/framed_helper.rs
+++ src/framed_helper.rs
@@ -1,17 +1,21 @@
 use tokio_core::io::FramedIo;
 use futures::{Future, Poll, Async};
 use std::io;
+use std::mem;
 
 pub fn framed_write<T: FramedIo>(io: T, frame: T::In) -> FramedWrite<T> {
-    FramedWrite {
-        io: Some(io),
+    FramedWrite::Writing {
+        io: io,
         frame: Some(frame),
     }
 }
 
-pub struct FramedWrite<T: FramedIo> {
-    io: Option<T>,
-    frame: Option<T::In>,
+pub enum FramedWrite<T: FramedIo> {
+    Writing {
+        io: T,
+        frame: Option<T::In>,
+    },
+    Empty,
 }
 
 impl<T: FramedIo> Future for FramedWrite<T> {
@@ -19,29 +23,44 @@ impl<T: FramedIo> Future for FramedWrite<T> {
     type Error = io::Error;
 
     fn poll(&mut self) -> Poll<T, io::Error> {
-        let mut io = self.io.take().expect("already resolved");
+        match *self {
+            FramedWrite::Writing { ref mut io, ref mut frame } => {
+                try!(io.flush());
+                if io.poll_write().is_not_ready() {
+                    return Ok(Async::NotReady);
+                } else {
+                    if let Some(frame) = frame.take() {
+                        try!(io.write(frame));
+                        try!(io.flush());
+                    } else {
+                        unreachable!()
+                    }
+                }
+            }
 
-        try!(io.flush());
+            FramedWrite::Empty => panic!("poll FramedWrite after it's done"),
+        }
 
-        if io.poll_write().is_not_ready() {
-            self.io = Some(io);
-            Ok(Async::NotReady)
-        } else {
-            try!(io.write(self.frame.take().expect("already resolved")));
-            try!(io.flush());
-            Ok(Async::Ready(io))
+        match mem::replace(self, FramedWrite::Empty) {
+            FramedWrite::Writing { io, ..} => Ok(Async::Ready(io)),
+            FramedWrite::Empty => unreachable!(),
         }
     }
 }
 
 pub fn framed_read<T: FramedIo>(io: T) -> FramedRead<T> {
-    FramedRead {
-        io: Some(io),
+    FramedRead::Reading {
+        io: io,
+        res: None,
     }
 }
 
-pub struct FramedRead<T: FramedIo> {
-    io: Option<T>,
+pub enum FramedRead<T: FramedIo> {
+    Reading {
+        io: T,
+        res: Option<T::Out>,
+    },
+    Empty,
 }
 
 impl<T: FramedIo> Future for FramedRead<T> {
@@ -49,17 +68,29 @@ impl<T: FramedIo> Future for FramedRead<T> {
     type Error = io::Error;
 
     fn poll(&mut self) -> Poll<(T, T::Out), io::Error> {
-        let mut io = self.io.take().expect("already resolved");
+        match *self {
+            FramedRead::Reading { ref mut io, ref mut res } => {
+                try!(io.flush());
 
-        try!(io.flush());
+                if io.poll_read().is_not_ready() {
+                    return Ok(Async::NotReady);
+                } else {
+                    if let Async::Ready(r) = try!(io.read()) {
+                        *res = Some(r);
+                    } else {
+                        return Ok(Async::NotReady);
+                    }
+                }
 
-        if io.poll_read().is_not_ready() {
-            self.io = Some(io);
-            Ok(Async::NotReady)
-        } else {
-            let r = try!(io.read());
-            try!(io.flush());
-            Ok(r.map(|res| (io, res)))
+                try!(io.flush());
+            }
+
+            FramedRead::Empty => panic!("poll FramedRead after it's done"),
+        }
+
+        match mem::replace(self, FramedRead::Empty) {
+            FramedRead::Reading { io, res: Some(r) } => Ok(Async::Ready((io, r))),
+            _ => unreachable!(),
         }
     }
 }
