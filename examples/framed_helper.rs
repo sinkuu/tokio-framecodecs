401f06180c8fd26b4d267f4fced44c344788a73a
sinkuu <sinkuupump@gmail.com>
2016-10-16 17:55:01 +0900

    Fix warnings

--- examples/framed_helper.rs
+++ examples/framed_helper.rs
@@ -7,7 +7,6 @@ use framecodecs::fixed_length::FixedLengthTransport;
 use framecodecs::framed_helper::{framed_write, framed_read};
 use tokio_core::net::TcpStream;
 use tokio_proto::pipeline::Frame;
-use std::io;
 
 fn main() {
     let mut core = tokio_core::reactor::Core::new().unwrap();
@@ -18,7 +17,7 @@ fn main() {
     let ts = FixedLengthTransport::new(tcp, 5);
     let ts = core.run(framed_write(ts, Frame::Message(vec![65, 66, 67, 68, 69]))).unwrap();
     let ts = core.run(framed_write(ts, Frame::Message(vec![70, 71, 72, 73, 74]))).unwrap();
-    let (ts, r) = core.run(framed_read(ts)).unwrap();
+    let (_ts, r) = core.run(framed_read(ts)).unwrap();
     println!("{:?}", r);
 }
 
